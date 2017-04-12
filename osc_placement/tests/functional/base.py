d6e223a191e51333426fa8d8e9d85b2a0b4fc77f
Roman Podoliaka <rpodolyaka@mirantis.com>
2017-04-12 19:04:05 +0300

    CLI for inventories
    Co-Authored-by: Andrey Volkov <avolkov@mirantis.com>
    Blueprint: placement-osc-plugin
    Change-Id: Ie46b6217fda65cb5d0f1379d0b4a986b4c30a3eb

--- osc_placement/tests/functional/base.py
+++ osc_placement/tests/functional/base.py
@@ -79,3 +79,31 @@ class BaseTestCase(base.BaseTestCase):
 
     def resource_provider_delete(self, uuid):
         return self.openstack('resource provider delete ' + uuid)
+
+    def resource_inventory_show(self, uuid, resource_class):
+        cmd = 'resource provider inventory show {uuid} {rc}'.format(
+            uuid=uuid, rc=resource_class
+        )
+        return self.openstack(cmd, use_json=True)
+
+    def resource_inventory_list(self, uuid):
+        return self.openstack('resource provider inventory list ' + uuid,
+                              use_json=True)
+
+    def resource_inventory_delete(self, uuid, resource_class):
+        cmd = 'resource provider inventory delete {uuid} {rc}'.format(
+            uuid=uuid, rc=resource_class
+        )
+        self.openstack(cmd)
+
+    def resource_inventory_set(self, uuid, *resources):
+        cmd = 'resource provider inventory set {uuid} {resources}'.format(
+            uuid=uuid, resources=' '.join(
+                ['--resource %s' % r for r in resources]))
+        return self.openstack(cmd, use_json=True)
+
+    def resource_inventory_class_set(self, uuid, resource_class, **kwargs):
+        opts = ['--%s=%s' % (k, v) for k, v in kwargs.items()]
+        cmd = 'resource provider inventory class set {uuid} {rc} {opts}'.\
+            format(uuid=uuid, rc=resource_class, opts=' '.join(opts))
+        return self.openstack(cmd, use_json=True)
