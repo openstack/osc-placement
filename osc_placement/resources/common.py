1f2fdddd9c111a12294d08f0f4826b74af2ec84c
songwenping <songwenping@inspur.com>
2021-10-25 07:20:35 +0000

    Remove usage of six
    Remove six-library Replace the following items with Python 3 style code.
    - six.raise_from
    - six.StringIO
    - six.moves
    - requirement.txt
    
    Change-Id: If3d810a1dbe7d506aa55cc07717babec8393b8c5

--- osc_placement/resources/common.py
+++ osc_placement/resources/common.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from six.moves.urllib import parse as urlparse
+from urllib import parse as urlparse
 
 
 def encode(value, encoding='utf-8'):
